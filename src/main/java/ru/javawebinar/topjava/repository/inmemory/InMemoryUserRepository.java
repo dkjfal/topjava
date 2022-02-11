5e4f5ad0e77e37ec6776e81669fb9325b900f596
lodjdheheu <lodjdheheu@gmail.com>
2022-02-11 17:57:06 +0300

    Prepare to HM02

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -7,8 +7,6 @@ import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import javax.jws.soap.SOAPBinding;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
