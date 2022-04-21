873f746230f2e121e20289d63bbb60a81d00be7e
lodjdheheu <lodjdheheu@gmail.com>
2022-04-21 10:59:22 +0300

    init

--- src/main/java/ru/javawebinar/topjava/View.java
+++
@@ -1,7 +0,0 @@
-package ru.javawebinar.topjava;
-
-import javax.validation.groups.Default;
-
-public class View {
-    public interface Persist extends Default {}
-}
--- src/main/java/ru/javawebinar/topjava/util/exception/ErrorType.java
+++
@@ -1,8 +0,0 @@
-package ru.javawebinar.topjava.util.exception;
-
-public enum ErrorType {
-    APP_ERROR,
-    DATA_NOT_FOUND,
-    DATA_ERROR,
-    VALIDATION_ERROR
-}
